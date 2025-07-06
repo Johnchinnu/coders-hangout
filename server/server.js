12dd85f70ca7697bf1cd26c9294bb2e16406533e
johnchinnu <johnchinnu50@gmail.com>
2025-07-06 18:09:09 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -140,12 +140,7 @@ io.on('connection', async (socket) => {
 
 
 // For production, serve static assets
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('/{*any}', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../','client', 'build', 'index.html'));
-    });
-}
+
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
