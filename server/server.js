a84e6332e23f3b7c7fa961625551c5fd4359052b
johnchinnu <johnchinnu50@gmail.com>
2025-07-06 18:36:36 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://coders-hangout.onrender.com", // Update this to your frontend URL
         methods: ["GET", "POST"],
         credentials: true
     }
